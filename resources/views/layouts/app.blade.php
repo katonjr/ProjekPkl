0e4d1ab26a62649da2ab18016c367a0245205589
katonjr <ciplukml80@gmail.com>
2024-05-02 16:01:43 +0700

    login dan register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -143,7 +143,7 @@
                     </div>
                     <div class="sb-sidenav-footer">
                         <div class="small">Logged in as:</div>
-                        Start Bootstrap
+                        {{ auth()->user()->name }}
                     </div>
                 </nav>
             </div>
